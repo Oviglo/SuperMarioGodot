e1926cd5614dbef494311ce9f70192a3799937c2
Oviglo <ovigne.loic@gmail.com>
2021-03-05 23:02:48 +0100

    Bonus bloc

---
+++ script/entity/BonusBloc.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+var is_jumping = true
+var jump_animation = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	
