f72792332fccf327b0a55b18d391a7c10d512469
Oviglo <ovigne.loic@gmail.com>
2021-03-06 12:35:58 +0100

    HUD

--- script/entity/BonusBloc.gd
+++ script/entity/BonusBloc.gd
@@ -1,7 +1,9 @@
-extends Area2D
+extends StaticBody2D
 
 var is_jumping = true
 var jump_animation = 0
+
+export (int, "Coin") var type = 0;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
