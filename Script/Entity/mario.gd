0df9ef10d7efaf855e93e14790e71efbf2ed8686
Oviglo <ovigne.loic@gmail.com>
2024-01-24 22:55:47 +0100

    jump

--- Script/Entity/mario.gd
+++ Script/Entity/mario.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
-@export var speed: int = 100
-@export var gravity: int = 100
-@export_range(0.0, 1.0) var friction = 0.1
-@export_range(0.0 , 1.0) var acceleration = 0.25
+@export var speed: int = 90
+@export var jump_speed = -300
+@export var gravity: int = 600
+@export_range(0.0, 1.0) var friction = 0.2
+@export_range(0.0 , 1.0) var acceleration = 0.15
 
 enum STATE_TYPE {
 	IDLE,
@@ -25,3 +26,6 @@ func _physics_process(delta) -> void:
 		velocity.x = lerp(velocity.x, 0.0, friction)
 	
 	move_and_slide()
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_speed
