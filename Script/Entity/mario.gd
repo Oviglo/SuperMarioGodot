9b432d016c91bdb4b85c3632f8781157587a269d
Oviglo <ovigne.loic@gmail.com>
2024-01-26 23:51:18 +0100

    run

--- Script/Entity/mario.gd
+++ Script/Entity/mario.gd
@@ -2,11 +2,20 @@ extends CharacterBody2D
 
 @onready var _animated_sprite := $AnimatedSprite2D
 
-@export var speed: int = 90
+@export var walk_speed: int = 90
 @export var jump_speed = -300
+@export var run_speed: int = 130
 @export var gravity: int = 600
 @export_range(0.0, 1.0) var friction = 0.2
 @export_range(0.0 , 1.0) var acceleration = 0.15
+
+var speed: int = walk_speed
+var movement_state: int = MOVEMENT_STATE.STOP
+var direction: float = 1
+var is_run: bool = false
+var is_skid: bool = false
+
+const RUN_ANIMATION_SCALE: float = 1.2
 
 enum MOVEMENT_STATE {
 	STOP,
@@ -19,25 +28,8 @@ enum ANIMATION {
 	IDLE,
 	WALK,
 	JUMP,
+	SKID,
 }
-
-var movement_state: int = MOVEMENT_STATE.STOP
-var direction: float = 1
-
-func _physics_process(delta) -> void:
-	
-	velocity.y += gravity * delta
-	
-	direction = Input.get_axis("walk_left", "walk_right")
-	if direction != 0:
-		walk()
-	else:
-		stop()
-	
-	move_and_slide()
-	
-	if Input.is_action_just_pressed("jump"):
-		jump()
 
 func walk() -> void:
 	velocity.x = lerp(velocity.x, direction * speed, acceleration)
@@ -55,6 +47,20 @@ func jump() -> void:
 	if is_on_floor():
 		velocity.y = jump_speed
 		movement_state = MOVEMENT_STATE.JUMP
+	
+func run_start() -> void:
+	is_run = true
+	speed = run_speed
+	
+func run_stop() -> void:
+	is_run = false
+	speed = walk_speed
+	
+func run_animation_speed():
+	if (is_run):
+		_animated_sprite.set_speed_scale(RUN_ANIMATION_SCALE)
+	else:
+		_animated_sprite.set_speed_scale(1)	
 
 func set_animation(name: ANIMATION) -> void:
 	match (name):
@@ -64,10 +70,39 @@ func set_animation(name: ANIMATION) -> void:
 			else:
 				_animated_sprite.play("Jump")
 		ANIMATION.WALK:
-			if is_on_floor(): 
+			if is_on_floor():
 				_animated_sprite.play("Walk")
+				run_animation_speed()
 			else:
 				_animated_sprite.play("Jump")
-	# Set direction	
-	if direction != 0 and movement_state != MOVEMENT_STATE.JUMP:
+		ANIMATION.SKID:
+			if is_on_floor():
+				_animated_sprite.play('Skid')
+	
+	generate_direction()
+		
+func generate_direction():
+	if direction != 0 && is_on_floor():
 		_animated_sprite.flip_h = direction < 0
+
+# ===== CharacterBody2D functions =====
+func _physics_process(delta) -> void:
+	
+	velocity.y += gravity * delta
+	
+	direction = Input.get_axis("walk_left", "walk_right")
+	if direction != 0:
+		walk()
+	else:
+		stop()
+	
+	move_and_slide()
+	
+	if Input.is_action_just_pressed("jump"):
+		jump()
+		
+	if Input.is_action_just_pressed('run'):
+		run_start()
+		
+	if Input.is_action_just_released('run'):
+		run_stop()
