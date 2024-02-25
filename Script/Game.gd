a572aaf93c2a6694c12f6026104eae93581592d7
Oviglo <ovigne.loic@gmail.com>
2024-02-25 15:00:46 +0100

    goomba

---
+++ Script/Game.gd
@@ -0,0 +1,6 @@
+class_name Game
+extends Node
+
+var life:int = 3
+var world:int = 1
+var level:int = 1
---
+++ Script/Level/Level.gd
@@ -0,0 +1,24 @@
+extends Game
+
+@onready var entities: TileMap = $Entities
+@onready var goomba = preload("res://Scene/Entity/Enemy/goomba.tscn")
+
+func _ready() -> void:
+	entities.hide()
+	init_entities()
+
+func init_entities() -> void:
+	for cell in entities.get_used_cells(0):
+		var data:TileData = entities.get_cell_tile_data(0, cell) 
+		var position: Vector2 = entities.map_to_local(cell)
+		var name: String = data.get_custom_data("name")
+		
+		var entity = null
+		match name:
+			"goomba":
+				entity = goomba.instantiate()
+				
+		if (entity != null):
+			entity.position = position
+			add_child(entity)			
+	pass
